9521dcd1020a70bac1352f98506df033240bd39c
Anthony Conti <awconti.dev@gmail.com>
2025-01-20 17:36:24 -0500

    Added missing comments on UserModel object.

--- Library/TalonRAG.Domain/Models/UserModel.cs
+++ Library/TalonRAG.Domain/Models/UserModel.cs
@@ -1,17 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TalonRAG.Domain.Models
+﻿namespace TalonRAG.Domain.Models
 {
+	/// <summary>
+	/// Model representing a user on behalf of the domain.
+	/// </summary>
 	public class UserModel
 	{
+		/// <summary>
+		/// The unique identifier of the user.
+		/// </summary>
 		public int Id { get; set; }
 
+		/// <summary>
+		/// The email address of the user.
+		/// </summary>
 		public string Email { get; set; } = string.Empty;
 
+		/// <summary>
+		/// The date and time the user was created.
+		/// </summary>
 		public DateTime CreateDate { get; set; }
 	}
 }
