d3c8ed545f226273b6d9b1a05ddd3a7f3f0f72cf
Anthony Conti <awconti.dev@gmail.com>
2025-02-03 08:55:14 -0500

    Replaced last message endpoints with query filter on existing user conversation endpoint.

--- TalonRAG.WebAPI/Controllers/UsersController.cs
+++ TalonRAG.WebAPI/Controllers/UsersController.cs
@@ -11,16 +11,12 @@ namespace TalonRAG.WebAPI.Controllers
 		private readonly IUserApiService _userApiService = userApiService;
 
 		[HttpGet("{id}/conversations")]
-		public async Task<ActionResult<IList<ConversationDto>>> GetConversationsByUserIdAsync(int id)
+		public async Task<ActionResult<IList<ConversationDto>>> GetConversationsByUserIdAsync(int id, bool lastMessagesOnly)
 		{
-			var response = await _userApiService.GetConversationsByUserIdAsync(id);
-			return StatusCode(StatusCodes.Status200OK, response);
-		}
-
-		[HttpGet("{id}/conversations/last-messages")]
-		public async Task<ActionResult<IList<ConversationDto>>> GetLastMessagesInConversationsByUserIdAsync(int id)
-		{
-			var response = await _userApiService.GetLastMessagesInConversationsByUserIdAsync(id);
+			var response =
+				lastMessagesOnly is false
+					? await _userApiService.GetConversationsByUserIdAsync(id)
+					: await _userApiService.GetLastMessagesInConversationsByUserIdAsync(id);
 			return StatusCode(StatusCodes.Status200OK, response);
 		}
 	}
